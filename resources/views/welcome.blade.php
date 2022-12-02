9fed4f083274d9a64b9941dc65f38f0e1ea89130
Jay200123 <kenken.ayonon@gmail.com>
2022-12-03 00:34:25 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
     <style>
+
+        body, html{
+            margin: 0;
+            padding: 0;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-image: url('images/background-test2.jpg');
+            height: 100%;
+
+        }
         .center{
             text-align: center;
         }
@@ -14,19 +24,22 @@
   bottom: 0;
   right: 0;
 }
+
     </style>
 
     <body style="background-color: #b3ffff">
         @include('layouts.master')
-        <h2><i class="fa fa-paw" aria-hidden="true"></i>Welcome to Acme Pet Clinic</h2>
-        <h4>We Care About Your Pet's Well Being</h4>
-        <p class="center">
+        <div class="center">
+        <h2><i class="fa fa-paw" aria-hidden="true"></i><strong>Welcome to Acme Pet Clinic</strong></h2>
+        <h4><strong>We Care About Your Pet's Well Being</strong></h4>
+        </div>
+        <!-- <p class="center">
             <img src="/images/logo2.jpg" width = "300px" height="300px" alt="logo.jpeg">
-        </p>
+        </p> -->
 
 
         <footer>
-        <h4 class ="text-center text-primary">Acme Pet Clinic All Rights Reserved @2022</h4>
+        <h4 style="color:black">Acme Pet Clinic All Rights Reserved @2022</h4>
         </footer>
     </body>
     
